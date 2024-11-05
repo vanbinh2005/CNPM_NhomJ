8374d93b1460376e0d5f81c45bb0538147c4109d
Binh <dovanbinh@1512gmail.com>
2024-11-05 21:42:52 +0700

    add Service

---
+++ KoiOderingSystemsServices/Interfaces/IAccountServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
+
+namespace KoiOderingSystemsServices.Interfaces
+{
+    public interface IAccountServices
+    {
+        Task<List<Account>> Account();
+    }
+}
