89be20016af0398f49fded60a25faab229d5cfff
ThànhĐạt <datvt1943@ut.edu.vn>
2024-11-07 23:20:48 +0700

    Add files via upload

---
+++ ToursRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace KoiOderingSystemsRepositories
+{
+    public class TourRepository : IToursRepository
+    {
+        private readonly KoiOrderingFarmDbContext _dbContext;
+
+         public TourRepository(KoiOrderingFarmDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<List<Account>> GetAllAccount()
+        {
+            return await _dbContext.Accounts.ToListAsync();
+        }
+    }
+
+
+
+
+}
