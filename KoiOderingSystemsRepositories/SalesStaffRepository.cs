0e4b35e2f34bbcc0432e3e7e9068ecdb4a6fc031
ThànhĐạt <datvt1943@ut.edu.vn>
2024-11-08 02:26:38 +0700

    SalesStaffRepository.cs

--- KoiOderingSystemsRepositories/SalesStaffRepository.cs
+++ KoiOderingSystemsRepositories/SalesStaffRepository.cs
@@ -1,12 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace KoiOderingSystemsRepositories
 {
-    internal class SalesStaffRepository
+   public class SaleStaffRepository : ISalesStaffRepository
     {
+        private readonly KoiOrderingFarmDbContext _dbContext;
+
+        public SaleStaffRepository(KoiOrderingFarmDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Account>> GetAllAccount()
+        {
+            return await _dbContext.Accounts.ToListAsync();
+        }
     }
 }
