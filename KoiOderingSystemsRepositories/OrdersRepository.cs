55f4bddf6946645015e7287fbbc2329599b96b3e
Phan <hoapm8386@ut.edu.vn>
2024-11-10 15:55:01 +0700

    Update OrdersRepository.cs

--- KoiOderingSystemsRepositories/OrdersRepository.cs
+++ KoiOderingSystemsRepositories/OrdersRepository.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 namespace KoiOderingSystemsRepositories
 {
-    internal class OrdersRepository
+    public class OrdersRepository : IOrdersRepository
     {
+        private readonly KoiOrderingFarmDbContext _dbContext;
+        public OrdersRepository(KoiOrderingFarmDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<Order>> GetAllOrders()
+        {
+            return await _dbContext.Orders.ToListAsync();
+        }
     }
 }
