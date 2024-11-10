f80317145b3b390c9870a5fb81e7225fb62b9746
Phan <hoapm8386@ut.edu.vn>
2024-11-10 15:55:20 +0700

    Update ProductRepository.cs

--- KoiOderingSystemsRepositories/ProductRepository.cs
+++ KoiOderingSystemsRepositories/ProductRepository.cs
@@ -1,12 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 namespace KoiOderingSystemsRepositories
 {
-    internal class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly KoiOrderingFarmDbContext _dbContext;
+        public ProductRepository(KoiOrderingFarmDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await _dbContext.Products.ToListAsync();
+        }
     }
 }
