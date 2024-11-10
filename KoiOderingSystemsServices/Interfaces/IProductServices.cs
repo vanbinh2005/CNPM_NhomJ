9baa0a63fd50046985f20ccabd63c48f10027fcf
Phan <hoapm8386@ut.edu.vn>
2024-11-10 15:57:01 +0700

    Update and rename IProduct.cs to IProductServices.cs

--- KoiOderingSystemsServices/Interfaces/IProductServices.cs
+++ KoiOderingSystemsServices/Interfaces/IProductServices.cs
@@ -1,12 +1,15 @@
-﻿using System;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
 
 namespace KoiOderingSystemsServices.Interfaces
 {
-    internal interface IProduct
+    internal interface IProductServices
     {
+        Task<List<Product>> products();
     }
 }
