293ab913d7f6f8031e21a190d89818f2a20c5be9
Phan <hoapm8386@ut.edu.vn>
2024-11-10 15:58:09 +0700

    Update ProductServices.cs

--- KoiOderingSystemsServices/Services/ProductServices.cs
+++ KoiOderingSystemsServices/Services/ProductServices.cs
@@ -1,12 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using KoiOderingSystemsServices.Interfaces;
 
 namespace KoiOderingSystemsServices.Services
 {
-    internal class ProductServices
+    internal class ProductServices : IProductServices
     {
+        private readonly IProductRepository _repository;
+        public ProductServices(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public Task<List<Product>> products()
+        {
+            return _repository.GetAllProducts();
+        }
     }
 }
