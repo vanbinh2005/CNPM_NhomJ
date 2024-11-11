b24324b801f812f3ecc5c6da635c5c9063c20ec7
NgocDiemTH <diemhln7431@ut.edu.vn>
2024-11-11 21:19:26 +0700

    Update ConsultingStaffRepository.cs

--- KoiOderingSystemsRepositories/ConsultingStaffRepository.cs
+++ KoiOderingSystemsRepositories/ConsultingStaffRepository.cs
@@ -1,12 +1,28 @@
-﻿using System;
+namespace KoiOderingSystemsRepositories
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace KoiOderingSystemsRepositories
-{
-    internal class ConsultingStaffRepository
+{ 
+    public class ConsultingstaffRepository : IConsultingstaffRepository
     {
+        private readonly KoiOderingFarmDbContext _dpContext;
+        public ConsultingstaffRepository(KoiOderingFarmDbContext dbContext)
+        {
+            _dpContext = dbContext;
+        }
+        public async Task<List<Consultingstaff>> GetAllConsultingstaff()
+        {
+            return await _dpContext.Consultingstaff.TolistAsync();
+        }
     }
 }
+
+
+
