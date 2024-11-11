22b3447c76be3a371c6e0c16f6cf2b750564c525
NgocDiemTH <diemhln7431@ut.edu.vn>
2024-11-11 21:41:02 +0700

    Update IConsultingStaffRepository.cs

--- KoiOderingSystemsRepositories/Interfaces/IConsultingStaffRepository.cs
+++ KoiOderingSystemsRepositories/Interfaces/IConsultingStaffRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
 
 namespace KoiOderingSystemsRepositories.Interfaces
 {
-    internal interface IConsultingStaffRepository
+    public interface IConsultingstaffRepository
     {
+        Task<List<Consultingstaff>> GetAllConsultingstaff();
+
     }
 }
