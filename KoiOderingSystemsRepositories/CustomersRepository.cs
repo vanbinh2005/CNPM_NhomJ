e8aa4f74f5c44df6f6cbe1375007f9829ecf4bdf
NgocDiemTH <diemhln7431@ut.edu.vn>
2024-11-11 21:32:14 +0700

    Update CustomersRepository.cs

--- KoiOderingSystemsRepositories/CustomersRepository.cs
+++ KoiOderingSystemsRepositories/CustomersRepository.cs
@@ -1,18 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Interfaces;
 using KoiOderingSystemsRepositories.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace KoiOderingSystemsRepositories
 {
     public class CustomersRepository : ICustomerRepository
     {
+        private readonly KoiOrderingFarmDbContext _dpContext;
 
-    }
-
-    public interface ICustomerRepository
-    {
+        public CustomersRepository(KoiOrderingFarmDbContext dpContext)
+        {
+            _dpContext = dpContext;
+        }
+        public async Task<List<Customer>> GetAllCustomer()
+        {
+            return await _dpContext.Customers.ToListAsync();
+        }
     }
 }
+
+
