dca7ca0e6bc9f645f96b1336f377356b3e90a8b0
NgocDiemTH <diemhln7431@ut.edu.vn>
2024-11-11 21:39:05 +0700

    Update CustomerServices.cs

--- KoiOderingSystemsServices/Services/CustomerServices.cs
+++ KoiOderingSystemsServices/Services/CustomerServices.cs
@@ -1,12 +1,25 @@
-﻿using System;
+using KoiOderingSystemsRepositories.Interfaces;
+using KoiOderingSystemsServices.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
 
-namespace KoiOderingSystemsServices.Services
+namespace KoiOderingSystemsServices
 {
-    internal class CustomerServices
+    public class CustomerServices : ICustomerServices
     {
+        private readonly ICustomerRepository _repository;
+        public CustomerServices(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+        public Task<List<Customer>> Customer()
+        {
+            return _repository.GetAllCustomer();
+        }
     }
 }
+
