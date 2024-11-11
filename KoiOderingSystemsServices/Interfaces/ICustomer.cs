05ea1ae39bbbe466f3a48f38b6bc993ecca54859
NgocDiemTH <diemhln7431@ut.edu.vn>
2024-11-11 21:36:21 +0700

    Update ICustomer.cs

--- KoiOderingSystemsServices/Interfaces/ICustomer.cs
+++ KoiOderingSystemsServices/Interfaces/ICustomer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using KoiOderingSystemsRepositories.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace KoiOderingSystemsServices.Interfaces
 {
-    internal interface ICustomer
+
+    public interface ICustomerServices
     {
+        Task<List<Customer>> Customer();
     }
 }
