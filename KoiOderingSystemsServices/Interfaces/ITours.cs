609d553bf53405cd85e45144d652689d83c254c9
vanbinh2005 <155176794+vanbinh2005@users.noreply.github.com>
2024-11-14 06:00:54 +0700

    Update ITours.cs

--- KoiOderingSystemsServices/Interfaces/ITours.cs
+++ KoiOderingSystemsServices/Interfaces/ITours.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using KoiOderingSystemsRepositories.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace KoiOderingSystemsServices.Interfaces
 {
-    internal interface ITours
+    public interface IToursService
     {
+        Task<IEnumerable<Tour>> SearchToursAsync(string departureCity, string destinationCity, DateTime departureDate);
     }
 }
