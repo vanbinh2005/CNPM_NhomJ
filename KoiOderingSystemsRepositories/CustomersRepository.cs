706f6595f796532b2b22d1e6587c422f1609c08f
vanbinh2005 <155176794+vanbinh2005@users.noreply.github.com>
2024-11-17 13:09:36 +0700

    Update CustomersRepository.cs

--- KoiOderingSystemsRepositories/CustomersRepository.cs
+++ KoiOderingSystemsRepositories/CustomersRepository.cs
@@ -7,20 +7,30 @@ using KoiOderingSystemsRepositories.Interfaces;
 using KoiOderingSystemsRepositories.Entities;
 using Microsoft.EntityFrameworkCore;
 
-namespace KoiOderingSystemsRepositories
+public class CustomersRepository : IAccountRepository
 {
-    public class CustomersRepository : ICustomerRepository
-    {
-        private readonly KoiOrderingFarmDbContext _dpContext;
+    private readonly KoiOrderingFarmDbContext _context;
 
-        public CustomersRepository(KoiOrderingFarmDbContext dpContext)
-        {
-            _dpContext = dpContext;
-        }
-        public async Task<List<Customer>> GetAllCustomer()
-        {
-            return await _dpContext.Customers.ToListAsync();
-        }
+    public CustomersRepository(KoiOrderingFarmDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Customer> AddCustomerAsync(Customer customer)
+    {
+        _context.Customers.Add(customer);
+        await _context.SaveChangesAsync();
+        return customer;
+    }
+
+    public Task<List<Account>> GetAllAccount()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Customer> GetCustomerByIdAsync(int id)
+    {
+        return await _context.Customers.FindAsync(id);
     }
 }
 
