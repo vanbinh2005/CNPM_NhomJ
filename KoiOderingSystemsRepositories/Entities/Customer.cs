4ede4b53cedfaf744aefa7da85c6d8f210782dc1
vanbinh2005 <155176794+vanbinh2005@users.noreply.github.com>
2024-11-17 13:07:55 +0700

    Update Customer.cs

--- KoiOderingSystemsRepositories/Entities/Customer.cs
+++ KoiOderingSystemsRepositories/Entities/Customer.cs
@@ -6,14 +6,12 @@ namespace KoiOderingSystemsRepositories.Entities;
 public partial class Customer
 {
     public int CustomerId { get; set; }
-
     public string Name { get; set; } = null!;
-
     public string Email { get; set; } = null!;
-
     public string PhoneNumber { get; set; } = null!;
-
     public string? Address { get; set; }
-
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+    public object Fullname { get; set; }
+    public object Password { get; set; }
+    public object Phone { get; set; }
 }
