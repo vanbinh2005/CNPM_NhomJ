c8b2225b0384f004e9814f0dfc8e614a69b1b97b
vanbinh2005 <155176794+vanbinh2005@users.noreply.github.com>
2024-11-19 21:56:27 +0700

    Add files via upload

---
+++ KoiOderingSystemsServices/AccountServices.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using KoiOderingSystemsServices.Interfaces;
+
+namespace KoiOderingSystemsServices 
+{
+    public class AccountServices : IAccountServices
+    {
+        private readonly IAccountRepository _repository;
+        public AccountServices(IAccountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<Account>> Account()
+        {
+           return _repository.GetAllAccount();
+        }
+    }
+}
---
+++ KoiOderingSystemsServices/DeliveringStaffServices.cs
@@ -0,0 +1,25 @@
+﻿using KoiOderingSystemsRepositories.Entities;
+using KoiOderingSystemsRepositories.Interfaces;
+using KoiOderingSystemsServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiOderingSystemsServices
+{
+    public class DeliveringStaffServices : IDeliveringStaffServices
+    {
+        private readonly IDeliveringStaffRepository _repository;
+        public DeliveringStaffServices(IDeliveringStaffRepository repository)
+        { 
+            _repository = repository;
+        }
+
+        public Task<List<Deliveringstaff>> DeliveringStaff()
+        {
+            return _repository.GetAllDeliveringstaff();
+        }
+    }
+}
